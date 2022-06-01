8611de852d0d10c2e71df9a1994f05d591f1c517
Peter Lee <peter.lee@trademe.co.nz>
2022-06-01 18:09:35 +1200

    Initial Commit

---
+++ Shared/SoTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoTApp.swift
+//  Shared
+//
+//  Created by Peter Lee on 1/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SoTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
