c21097a5ffb2f1beb5d62d8c88c595c80a74168a
Peter Lee <leepete.nz@gmail.com>
2022-06-06 00:20:38 +1200

    Add colour helpers

---
+++ Shared/Colour+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Colour+Extension.swift
+//  SoTDesignDevBootcamp2022
+//
+//  Created by Peter Lee on 5/06/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandMasterPrimary = Color("BrandMasterPrimary")
+    static let brandMasterSecondary = Color("BrandMasterSecondary")
+    static let brandPrimary = Color("BrandPrimary")
+    static let brandSecondary = Color("BrandSecondary")
+}
